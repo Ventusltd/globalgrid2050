4203360a5a61afd5f4f310e7d5569bd0ab23335a
Ventusltd <vikram.kumar@ventusltd.com>
2026-05-29 23:50:53 +0100

    Add V6 live data config module

---
+++ uk_energy_tracking_v6/live_data_pipeline/live-config.js
@@ -0,0 +1,8 @@
+window.V6LiveConfig={
+  energy:'/uk_energy_tracking_v6/live_grid_energy.json',
+  price:'/uk_energy_tracking_v6/live_grid_price.json',
+  commodities:'/uk_energy_tracking_v6/live_oil_prices.json',
+  priceHistory:'/uk_energy_tracking_v6/electricity_price_history.json',
+  dailyPriceHistory:'/uk_energy_tracking_v6/electricity_price_history_daily_decade.json',
+  annualBase:'/data/electricity/elexon_system_prices_'
+};
